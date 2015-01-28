46b8aa8ed67f94b02e0bc782b25af771dfc0da18
Alusor <eddvelez@outlook.com>
2015-01-28 00:45:56 -0600

    Carga del Diccionario a memoria

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 #Carga del diccionario de mnemonicos
 mnemonicos = File.open("mnemonicos.dic","r")
-
-
-
+hash = mnemonicos.read
+puts hash.class
+hash = hash.split(',')
+puts hash.class
+puts  hash[5]
